0647bc0324a050e37a55715ca6ff664d1fc33350
unknown <cmwillett87@gmail.com>
2021-05-26 12:14:09 -0400

    Created sweepstake class in sweeepstake.py, need to define the methods still.

---
+++ sweepstake.py
@@ -0,0 +1,20 @@
+class Sweepstake:
+
+    def __init__(self, name):
+        self.name = name
+        self.contestants = {}
+
+    def sweepstake(self):
+        # init self passing in name
+        pass
+
+    def register_contestant(self, contestant):
+        pass
+
+    def pick_winner(self):
+        #call notify winner method from Contestant class
+        pass
+
+    def print_contestant_info(self, contestant):
+        #def print method in user interface and method here
+        pass
