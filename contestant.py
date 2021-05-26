1e48bca1fa9a17348f0c153ae279c149fa06bc1e
unknown <cmwillett87@gmail.com>
2021-05-26 11:50:43 -0400

    Created the contestant class in contestant.py, contestants havea first name, last name, email and registration number.  Methods still need to be defined.

---
+++ contestant.py
@@ -0,0 +1,15 @@
+class Contestant:
+
+    def __init__(self, first, last, email, number):
+        self.first_name = first
+        self.last_name = last
+        self.email = email
+        self.reg_number = number
+
+    def contestant(self):
+        #init self passing in first, last, email, number
+        pass
+
+    def notify(self):
+        #notifys winner calling method from user interface
+        pass
