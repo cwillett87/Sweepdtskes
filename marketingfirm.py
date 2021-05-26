d43ac46c86189943ec7b966ef7df3a1ab284b01f
unknown <cmwillett87@gmail.com>
2021-05-26 12:25:18 -0400

    Created a marketing firm class in marketingfirm.py.  Still need to define the methods.

---
+++ marketingfirm.py
@@ -0,0 +1,10 @@
+class Marketing_firm:
+
+    def __init__(self):
+        self.manager = ''
+
+    def marketing_firm(self, manager):
+        pass
+
+    def create_sweepstakes(self):
+        pass
